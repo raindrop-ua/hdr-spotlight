988338db9fc18deaf4e91a50db6397d43a7cf64c
Anton Sizov <raindropua@gmail.com>
2026-09-13 09:32:09 +0300

    chore: update SiteFooter styles and markup for streamlined structure and improved utility class usage

--- src/app/shared/ui/site-footer/site-footer.component.ts
+++ src/app/shared/ui/site-footer/site-footer.component.ts
@@ -2,6 +2,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-site-footer',
+  host: { class: 'block' },
   templateUrl: './site-footer.component.html',
   styleUrl: './site-footer.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,

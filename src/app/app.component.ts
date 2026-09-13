eb08db9edd8a728c55fd448a356e607c239a16eb
Anton Sizov <raindropua@gmail.com>
2026-09-13 09:24:40 +0300

    feat: integrate SiteFooter into app component and template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
+import { SiteFooter } from '@shared/ui/site-footer/site-footer.component';
 import { Component } from '@angular/core';
 import { Bench } from '@features/bench/bench.component';
 
 @Component({
   selector: 'app-root',
-  imports: [Bench],
+  imports: [Bench, SiteFooter],
   templateUrl: './app.component.html',
 })
 export class App {}
